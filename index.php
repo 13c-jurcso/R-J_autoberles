87a65d6e4be81d78cd853197ae2a299a81021d6d
13c-jurcso@ipari.vein.hu <13c-jurcso@ipari.vein.hu>
2024-11-05 10:46:14 +0100

    index frissites

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>R&J</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
 <header>
@@ -24,13 +25,34 @@
         </ul>
     </nav>
 </header>
-
+<form>
+        <div id="torzs">
+          <h1>R&J autókölcsönző. Indulás!</h1>
+          <div id="kezdo_input">
+          <select name="hely" id="hely">
+            <option value="hely">Veszprém</option>
+            <option value="hely">Budapest</option>
+            <option value="hely">Debrecen</option>
+          </select>
+          <input type="datetime-local" name="atvetel" id="atvetel">
+          <input type="datetime-local" name="leadas" id="leadas">
+          <input type="submit" id="submit_jarmuvek" value="Járművek megtekintése">
+          </div>
+        </div>
+      </form>
 
 
 <script>
     document.querySelector(".menu-toggle").addEventListener("click", function () {
         document.querySelector("header").classList.toggle("menu-opened");
     });
+    const ido = new Date();
+        let felvetel = Date.parse(document.getElementById("felvetel").value);
+        let felvetel_ido = new Date(felvetel);
+        if (felvetel < ido.getTime() || isNaN(felvetel)) return alert("A felvételi idő nem lehet korábban a jelenlegi időnél!");
+        let leadas = Date.parse(document.getElementById("leadas").value);
+        let leadas_ido = new Date(leadas);
+        if (leadas < felvetel || isNaN(leadas)) return alert("A leadási idő nem lehet hamarabb, mint a felvételi idő!");
 </script>
 
 </body>
