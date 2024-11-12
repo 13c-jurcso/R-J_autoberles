62082885413911543ccae02efe01192609c1d419
13c-jurcso <13c-jurcso@ipari.vein.hu>
2024-11-12 10:44:18 +0100

    connect fail update

--- db_connection.php
+++ db_connection.php
@@ -9,7 +9,7 @@ $db = new mysqli($servername, $username, $password, $dbname);
 
 // Kapcsolat ellenőrzése
 if ($db->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $db->connect_error);
 }
 $db->set_charset("utf8mb4");
 ?>
