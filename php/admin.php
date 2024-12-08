eabba16ecf8fb5acc182c742edc6a292bc50089e
13c-david <13c-david@ipari.vein.hu>
2024-12-08 17:59:52 +0100

    bérlés inputok - de még dolgozni kell rajta

--- php/admin.php
+++ php/admin.php
@@ -15,6 +15,7 @@ $felhasznalok = $db->query("SELECT * FROM felhasznalo;");
     <title>Vezérlőpult</title>
     <link rel="stylesheet" href="../css/admin.css">
     <link rel="stylesheet" href="../css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <header>
