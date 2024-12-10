e3cc0cefe74fe16a5941c6e0b94bfe85e1c432f5
13c-jurcso <13c-jurcso@ipari.vein.hu>
2024-12-10 10:47:30 +0100

    stílus husegpontok

--- php/husegpontok.php
+++ php/husegpontok.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Husegpontok</title>
+    <link rel="stylesheet" href="../css/husegpontok.css">
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="../css/index.css">
 
@@ -27,20 +28,21 @@
     </nav>
 </header>
 <div class="container">
-<h2>Hűségpontok</h2>
-<p>A hűségpontok rendszere lehetőséget biztosít arra, hogy vásárlásaid után értékes jutalmakat szerezz anélkül, hogy bármilyen külön regisztrációra lenne szükség. Az egész folyamat rendkívül egyszerű és kényelmes: mindössze annyi szükséges, hogy a vásárlásaid során a rendszer automatikusan rögzítse a szükséges adatokat, és a pontok maguktól gyűlnek, hogy később kihasználhasd őket.
+    <h2>Hűségpontok</h2>
+    <p>A hűségpontok rendszere lehetőséget biztosít arra, hogy vásárlásaid után értékes jutalmakat szerezz anélkül, hogy bármilyen külön regisztrációra lenne szükség. Az egész folyamat rendkívül egyszerű és kényelmes: mindössze annyi szükséges, hogy a vásárlásaid során a rendszer automatikusan rögzítse a szükséges adatokat, és a pontok maguktól gyűlnek, hogy később kihasználhasd őket.</p>
 
-**Hogyan működik a rendszer?**
+    <p><b>Hogyan működik a rendszer?</b></p>
+    <ol>
+    <li><b>Pontok gyűjtése:</b> Minden vásárlás során egy előre meghatározott arányban hűségpontokat kapsz. A pontok száma a vásárlásaid összegétől függ – minél többet vásárolsz, annál több pontot gyűjtesz. A rendszer azonnal rögzíti az adatokat, így nem kell semmit külön tenned.</li>
 
-1. **Pontok gyűjtése**: Minden vásárlás során egy előre meghatározott arányban hűségpontokat kapsz. A pontok száma a vásárlásaid összegétől függ – minél többet vásárolsz, annál több pontot gyűjtesz. A rendszer azonnal rögzíti az adatokat, így nem kell semmit külön tenned.
+    <li><b>Nincs szükség regisztrációra:</b> A legjobb az egészben, hogy nem szükséges semmilyen extra lépés vagy regisztráció a programhoz való csatlakozáshoz. Minden vásárlás során automatikusan hozzáadódnak a pontok a vásárlói fiókodhoz, így nem kell aggódnod a külön regisztrációs folyamatok miatt. Az adatok az üzletnél történő vásárláskor kerülnek rögzítésre, és a pontjaid is automatikusan frissülnek.</li>
 
-2. **Nincs szükség regisztrációra**: A legjobb az egészben, hogy nem szükséges semmilyen extra lépés vagy regisztráció a programhoz való csatlakozáshoz. Minden vásárlás során automatikusan hozzáadódnak a pontok a vásárlói fiókodhoz, így nem kell aggódnod a külön regisztrációs folyamatok miatt. Az adatok az üzletnél történő vásárláskor kerülnek rögzítésre, és a pontjaid is automatikusan frissülnek.
+    <li><b>Pontok felhasználása:</b> A felhalmozott pontokat később különféle kedvezményekre, ajándékokra vagy exkluzív ajánlatokra válthatod be. A pontok felhasználása szintén egyszerű: a vásárláskor a rendszer automatikusan felajánlja a pontjaid felhasználásának lehetőségét, így még kényelmesebbé téve a vásárlásokat.</li>
 
-3. **Pontok felhasználása**: A felhalmozott pontokat később különféle kedvezményekre, ajándékokra vagy exkluzív ajánlatokra válthatod be. A pontok felhasználása szintén egyszerű: a vásárláskor a rendszer automatikusan felajánlja a pontjaid felhasználásának lehetőségét, így még kényelmesebbé téve a vásárlásokat.
+    <li><b>Egyszerű és gyors:</b> Mivel nincs szükség külön regisztrációra, a hűségpontok gyűjtése és felhasználása minden vásárlásnál zökkenőmentes és gyors. Csak vásárolj, és élvezd a hűségpontok előnyeit, miközben azokat bármikor felhasználhatod a következő vásárláskor.</li>
+    </ol>
 
-4. **Egyszerű és gyors**: Mivel nincs szükség külön regisztrációra, a hűségpontok gyűjtése és felhasználása minden vásárlásnál zökkenőmentes és gyors. Csak vásárolj, és élvezd a hűségpontok előnyeit, miközben azokat bármikor felhasználhatod a következő vásárláskor.
-
-A hűségpontok rendszere tehát egy kényelmes és egyszerű módja annak, hogy hűségedet jutalmakkal viszonozzuk, anélkül, hogy bárminemű adminisztrációval kellene foglalkoznod. Vásárolj bátran, és gyűjtsd a pontokat, hogy a következő vásárlásod még kellemesebb élmény legyen!</p>
+    <p>A hűségpontok rendszere tehát egy kényelmes és egyszerű módja annak, hogy hűségedet jutalmakkal viszonozzuk, anélkül, hogy bárminemű adminisztrációval kellene foglalkoznod. Vásárolj bátran, és gyűjtsd a pontokat, hogy a következő vásárlásod még kellemesebb élmény legyen!</p>
 </div>
 
 
